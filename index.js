841812c1c09abe570d75da64e6cde740988392ea
Andzej-S <andrewstreckul@gmail.com>
2023-02-26 21:00:29 +0000

    add questions js in lib folder and added a link in index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const questions = require('./lib/questions');
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
-
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
